5ed28cddaa0db2259a3a58406df9589b9964e092
Franck Quasevi <franckcode6@gmail.com>
2022-05-11 22:26:55 +0200

    AjoutDonnesInitiales Emotions/Utilisateurs/Themes/Jours

--- src/main/java/fr/humanbooster/fx/calendrier_gif/initialisation/AjoutDonneesInitiales.java
+++ src/main/java/fr/humanbooster/fx/calendrier_gif/initialisation/AjoutDonneesInitiales.java
@@ -2,13 +2,11 @@ package fr.humanbooster.fx.calendrier_gif.initialisation;
 
 import java.time.LocalDate;
 import java.util.Locale;
+import java.util.Random;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-
 import com.github.javafaker.Faker;
-import com.github.javafaker.service.FakeValuesService;
-import com.github.javafaker.service.RandomService;
 
 import fr.humanbooster.fx.calendrier_gif.business.Emotion;
 import fr.humanbooster.fx.calendrier_gif.business.Jour;
@@ -30,26 +28,27 @@ public class AjoutDonneesInitiales implements CommandLineRunner {
 	private final JourDao jourDao;
 	private final UtilisateurDao utilisateurDao;
 
-	private static FakeValuesService fakeValuesService = new FakeValuesService(new Locale("fr-FR"),
-			new RandomService());
 	private final Faker faker = new Faker(new Locale("fr-FR"));
+	private static Random random = new Random();
 
 	@Override
 	public void run(String... args) throws Exception {
 
-		System.out.println("Ajout");
+		//EMOTIONS
+		System.out.println("Ajout des émotions");
 		emotionDao.save(new Emotion("Souriant", "&#x1F600;"));
 		emotionDao.save(new Emotion("Monocle", "&#x1F9D0;"));
 		emotionDao.save(new Emotion("Bisous", "&#x1F618;"));
 		emotionDao.save(new Emotion("Coeur", "&#x1F60D;"));
 		emotionDao.save(new Emotion("PTDR", "&#x1F923;"));
 
-		System.out.println("ajout themes:");
+		//THEMES
+		System.out.println("Ajout des thèmes");
 		themeDao.save(new Theme("dark"));
 		themeDao.save(new Theme("bachata"));
-		themeDao.save(new Theme("bootstrap"));
 
-		System.out.println("ajout jours:");
+		//JOURS
+		System.out.println("Ajout des jours");
 		int anneeEnCours = LocalDate.now().getYear();
 		int moisEnCours = LocalDate.now().getMonthValue();
 		LocalDate localDate = LocalDate.of(anneeEnCours, moisEnCours, 1);
@@ -59,18 +58,18 @@ public class AjoutDonneesInitiales implements CommandLineRunner {
 			localDate = localDate.plusDays(1);
 		}
 
-		System.out.println("ajout utilisateurs");
-
-		utilisateurDao.save(new Utilisateur("Quasevi", "Franck", "franck@hb.com", "12345", themeDao.getById(1L)));
-
-		utilisateurDao.save(new Utilisateur("Toto", "Titi", "titi@hb.com", "12345", themeDao.getById(2L)));
-
-		utilisateurDao.save(new Utilisateur("Choquet", "Laurent", "laurent@hb.com", "12345", themeDao.getById(2L)));
-
-		utilisateurDao.save(new Utilisateur("Gomez", "Jeanne", "jeanne@hb.com", "12345", themeDao.getById(1L)));
-
-		utilisateurDao.save(new Utilisateur(faker.name().lastName(), faker.name().firstName(), "john@hb.com", "12345",
-				themeDao.getById(1L)));
-
+		//UTILISATEURS
+		System.out.println("Ajout des utilisateurs");
+		utilisateurDao.save(new Utilisateur("Quasevi", "Franck", "franck@hb.com", "12345", themeDao.getById(2L)));
+		//Creation du boucle pour générer des utilisateurs aléatoires avec thème1
+		for (int i = 0; i < 5; i++) {
+			utilisateurDao.save(new Utilisateur(faker.name().lastName(), faker.name().firstName(),
+					faker.internet().emailAddress(), "12345", themeDao.getById(1L)));
+		}
+		//Creation du boucle pour générer des utilisateurs aléatoires avec thème2
+		for (int i = 0; i < 5; i++) {
+			utilisateurDao.save(new Utilisateur(faker.name().lastName(), faker.name().firstName(),
+					faker.internet().emailAddress(), "12345", themeDao.getById(2L)));
+		}
 	}
 }
