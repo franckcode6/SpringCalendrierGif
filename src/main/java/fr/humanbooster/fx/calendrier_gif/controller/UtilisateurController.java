9faa294435b4e2158b351180b9f1a1ab7618f9e4
Franck Quasevi <franckcode6@gmail.com>
2022-05-17 09:26:37 +0200

    Modification des controllers + BS formulaire inscription

--- src/main/java/fr/humanbooster/fx/calendrier_gif/controller/UtilisateurController.java
+++ src/main/java/fr/humanbooster/fx/calendrier_gif/controller/UtilisateurController.java
@@ -1,22 +1,28 @@
 package fr.humanbooster.fx.calendrier_gif.controller;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import fr.humanbooster.fx.calendrier_gif.business.Utilisateur;
+import fr.humanbooster.fx.calendrier_gif.service.ThemeService;
 import fr.humanbooster.fx.calendrier_gif.service.UtilisateurService;
 import lombok.AllArgsConstructor;
 
 @Controller
 @AllArgsConstructor
-public class IndexController {
+public class UtilisateurController {
 
 	private final UtilisateurService utilisateurService;
 	private final HttpServletRequest request;
+	private final ThemeService themeService;
 
 	// Toutes les méthodes de ce controller devront renvoyer un objet de type
 	// ModelAndView
@@ -25,7 +31,7 @@ public class IndexController {
 	// Autrement dit quelle(s) sont la ou les URL(s) que la méthode prend en charge
 	// Equivalent de @WebServlet
 	@GetMapping({ "/index", "/" })
-	public ModelAndView index() {
+	public ModelAndView connexionGet() {
 		// On déclare et on instancie un objet de type ModelAndView
 		ModelAndView mav = new ModelAndView();
 
@@ -50,7 +56,7 @@ public class IndexController {
 	 * @return
 	 */
 	@PostMapping({ "/index", "/" })
-	public ModelAndView indexPost(@RequestParam(name = "EMAIL") String email,
+	public ModelAndView connexionPost(@RequestParam(name = "EMAIL") String email,
 			@RequestParam(name = "MOT_DE_PASSE") String motDePasse) {
 		if (utilisateurService.recupererUtilisateur(email, motDePasse) == null) {
 			return new ModelAndView("redirect:index");
@@ -60,6 +66,57 @@ public class IndexController {
 			System.out.println("CONNEXION OK");
 			return new ModelAndView("redirect:calendrier");
 		}
-
 	}
+
+	/**
+	 * Méthode permettant de déconnecter l'utilisateur et de le rediriger vers la
+	 * page index
+	 * 
+	 * @return
+	 */
+	@GetMapping("deconnexion")
+	public ModelAndView deconnexionGet() {
+		request.getSession().invalidate();
+		return new ModelAndView("redirect:index");
+	}
+
+	/**
+	 * Principe Hollywood; ne nous appelez pas, nous vous appelerons
+	 * 
+	 * @param utilisateur
+	 * @return
+	 */
+	@GetMapping("inscription")
+	public ModelAndView inscriptionGet(@ModelAttribute Utilisateur utilisateur) {
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("theme", themeService.recupererThemes());
+
+		mav.setViewName("inscription");
+
+		return mav;
+	}
+
+	/**
+	 * Méthode invoquée quand l'utilisateur clique sur le bouton inscription
+	 * 
+	 * @param utilisateur
+	 * @param result
+	 * @return
+	 */
+	@PostMapping("inscription")
+	public ModelAndView inscriptionPost(@Valid @ModelAttribute("utilisateur") Utilisateur utilisateur,
+			BindingResult result) {
+
+		if (result.hasErrors()) {
+			// La validation de l’objet utilisateur par rapport aux contraintes
+			// de validation définies dans la classe métier Utilisateur a produit
+			// des erreurs
+			ModelAndView mav = inscriptionGet(utilisateur);
+			return mav;
+		} else {
+			utilisateurService.ajouterUtilisateur(utilisateur);
+			return new ModelAndView("redirect:index");
+		}
+	}
+
 }
--- src/main/webapp/WEB-INF/inscription.jsp
+++ src/main/webapp/WEB-INF/inscription.jsp
@@ -20,25 +20,25 @@
 	<jsp:include page="hautDePage.jsp"></jsp:include>
 	<main class="container">
 		<form:form modelAttribute="utilisateur" action="inscription" method="post">
-	<form:label path="nom">Nom</form:label>
-	<form:input path="nom" />
+	<form:label class="form-label" path="nom">Nom</form:label>
+	<form:input class="form-control" path="nom" />
 	<form:errors path="nom" cssClass="erreur" /><br>
-	<form:label path="prenom">Prénom</form:label>
-	<form:input path="prenom" />
+	<form:label class="form-label" path="prenom">Prénom</form:label>
+	<form:input class="form-control" path="prenom" />
 	<form:errors path="prenom" cssClass="erreur" /><br>
-	<form:label path="email">Email</form:label>
-	<form:input path="email" />
+	<form:label class="form-label" path="email">Email</form:label>
+	<form:input class="form-control" path="email" />
 	<form:errors path="email" cssClass="erreur" /><br>
-	<form:label path="motDePasse">Mot de passe</form:label>
-	<form:password path="motDePasse" />
+	<form:label class="form-label" path="motDePasse">Mot de passe</form:label>
+	<form:password class="form-control" path="motDePasse" />
 	<form:errors path="motDePasse" cssClass="erreur" /><br>
-	<form:label path="theme">Thème</form:label>
-	<form:select path="theme">
+	<form:label class="form-label" path="theme">Thème</form:label>
+	<form:select class="form-select" path="theme">
 		<form:option value="0">Merci de choisir un thème</form:option>
 		<form:options items="${theme}" itemValue="id" itemLabel="nom"/>
 	</form:select>
 	<form:errors path="theme" cssClass="erreur" /><br>
-	<form:button>Inscription</form:button>
+	<form:button class="btn btn-success">Inscription</form:button>
 </form:form>
 	</main>
 
