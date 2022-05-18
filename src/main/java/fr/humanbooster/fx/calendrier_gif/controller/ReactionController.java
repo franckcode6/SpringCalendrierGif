1b646a57ce5780340e061f38883442afae6de02e
Franck Quasevi <franckcode6@gmail.com>
2022-05-18 12:07:43 +0200

    Mise en place reactions

--- src/main/java/fr/humanbooster/fx/calendrier_gif/controller/ReactionController.java
+++ src/main/java/fr/humanbooster/fx/calendrier_gif/controller/ReactionController.java
@@ -1,21 +1,29 @@
 package fr.humanbooster.fx.calendrier_gif.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import fr.humanbooster.fx.calendrier_gif.business.Gif;
+import fr.humanbooster.fx.calendrier_gif.business.Utilisateur;
 import fr.humanbooster.fx.calendrier_gif.service.EmotionService;
 import fr.humanbooster.fx.calendrier_gif.service.GifService;
+import fr.humanbooster.fx.calendrier_gif.service.ReactionService;
 import lombok.AllArgsConstructor;
 
 @Controller
 @AllArgsConstructor
 public class ReactionController {
 	
-	public final GifService gifService;
-	public final EmotionService emotionService;
+	private final GifService gifService;
+	private final ReactionService reactionService;
+	private final EmotionService emotionService;
+	private final HttpSession httpSession;
+	
 	
 	@GetMapping("reaction")
 	public ModelAndView reactionsGet(@RequestParam(name="gif_id") Long id) {
@@ -24,12 +32,21 @@ public class ReactionController {
 		Gif gif = gifService.recupererGif(id);
 		
 		mav.addObject("gif", gif);
-		
 		mav.addObject("emotions", emotionService.recupererEmotions());
-		
 		mav.setViewName("reaction");
 		
 		return mav;
+	}
+	
+	@PostMapping("reaction")
+	public ModelAndView reactionPost(@RequestParam(name="gif_id") Long id,
+			@RequestParam(name="emotion_id") Long emotionId) {
+		Utilisateur utilisateur = (Utilisateur) httpSession.getAttribute("utilisateur");
+		
+		reactionService.ajouterReaction(gifService.recupererGif(id), emotionService.recupererEmotion(emotionId),
+				utilisateur);
+		
+		return new ModelAndView("redirect:calendrier");
 	}
 
 }
