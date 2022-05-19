2db01591078a99dcea3ee0d2c1a9315d6418f2fa
Franck Quasevi <franckcode6@gmail.com>
2022-05-19 09:40:25 +0200

    Rangement controllers

--- src/main/java/fr/humanbooster/fx/calendrier_gif/controller/CalendrierGifController.java
+++ src/main/java/fr/humanbooster/fx/calendrier_gif/controller/CalendrierGifController.java
@@ -10,6 +10,8 @@ import java.time.LocalDate;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,15 +26,36 @@ import lombok.AllArgsConstructor;
 
 @Controller
 @AllArgsConstructor
-public class GifController {
+public class CalendrierGifController {
 
 	protected static final String DOSSIER_IMAGES = "src/main/webapp/images/";
+	private static final int NB_JOURS_PAR_PAGE = 7;
+	
 	private static Long compteurGif = 1L;
-
+	
 	private final GifService gifService;
-	private final JourService jourService;
 	private final HttpSession httpSession;
-
+	private final JourService jourService;
+	
+	/**
+	 * Méthode permettant de générer la page de calendrier On y inclut la totalité
+	 * des jours en BDD
+	 * 
+	 * @return
+	 */
+	@GetMapping("calendrier")
+	public ModelAndView calendrierGet(@PageableDefault(size = NB_JOURS_PAR_PAGE, sort = "date") Pageable pageable) {
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("jours", jourService.recupererJours());
+		mav.setViewName("calendrier");
+		mav.addObject("pageDeJours", jourService.recupererJours(pageable));
+		// Met en session la page choisie
+		if (pageable!=null) {
+			httpSession.setAttribute("numeroDePage", pageable.getPageNumber());
+		}
+		return mav;
+	}
+	
 	/**
 	 * Affichage de la vue gifDistant
 	 * 
